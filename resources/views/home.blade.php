e33c5077ad425708923784ba2380dea89f84cd99
jeremy chang <jeremy.chang@lmdsio.fr>
2024-10-03 16:46:40 +0200

    Initial commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,9 @@
+@extends("layouts.master")
+@section("content")
+@stop
+
+<div>
+    <h1 class="bvn">Gestion des frais de déplacements</h1>
+</div>
+
+
---
+++ resources/views/vues/error.blade.php
@@ -0,0 +1,5 @@
+    @if ($erreur != "")
+            <div class="alert-danger" role="alert">
+                <span class="glyphicon glyphicon-exclamation-sign" aria-hidden="true">{{$erreur}}</span>
+            </div>
+    @endif
